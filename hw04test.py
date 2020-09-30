501527e771c6dea0e0944753f99faffc0d94bace
DChen810 <65102644+DChen810@users.noreply.github.com>
2020-09-30 01:39:35 -0700

    Update hw04test.py

--- hw04test.py
+++ hw04test.py
@@ -4,7 +4,7 @@ from hw04 import getRepo, getCommit
 
 class TestgetInfo(unittest.TestCase):
     def test_get_repo_01(self):
-        expectresult = ['Annie', 'birthday', 'ssw567', 'Student-Repository', 'Triangle567'] 
+        expectresult = ['Annie', 'birthday', 'hw04','ssw567', 'Student-Repository', 'Triangle567'] 
         self.assertEqual(getRepo('dchen810'), expectresult)
         self.assertEqual(getRepo('dchen810'), expectresult)
 
@@ -13,6 +13,7 @@ class TestgetInfo(unittest.TestCase):
         name = 'dchen810'
         self.assertEqual( getCommit(name, 'Annie'), 2)
         self.assertEqual( getCommit(name, 'birthday'), 1)
+        self.assertEqual( getCommit(name, 'hw04'), 12)
         self.assertEqual( getCommit(name, 'ssw567'), 4)
         self.assertEqual( getCommit(name, 'Student-Repository'), 7)
         self.assertEqual( getCommit(name, 'Triangle567'), 4)
